40f5fc0ce2a9909bbbbfab1b37d80f9216380d49
Philip Sather <pasather83@gmail.com>
2026-06-26 14:08:27 +0400

    Clean deploy latest Ziggy static app

--- backend/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from app.routes import app as ziggy_routes
-
-app = Flask(__name__)
-app.register_blueprint(ziggy_routes)
-
-if __name__ == '__main__':
-    app.run(debug=True)
--- backend/app/utils.py
+++
@@ -1,6 +0,0 @@
-def find_answer(user_input, kb):
-    for category in kb:
-        for key in kb[category]:
-            if key in user_input:
-                return kb[category][key]
-    return "Hmm... Ziggy doesn't know that yet. Ask HR maybe? 😉"
